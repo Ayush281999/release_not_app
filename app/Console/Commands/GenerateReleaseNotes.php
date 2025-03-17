8f49021e2c8a565be83d4350d24c9aef10cb9755
Ayush Kumar <ayushk.itpath@gmail.com>
2025-03-17 17:26:37 +0530

    improve the code structure -im

--- app/Console/Commands/GenerateReleaseNotes.php
+++ app/Console/Commands/GenerateReleaseNotes.php
@@ -60,7 +60,6 @@ class GenerateReleaseNotes extends Command
             'changes' => [],
             'improvements' => [],
         ];
-        
         foreach ($commits as $commit) {
             $message = $commit['commit']['message'];
             if (str_contains($message, '-bf')) {
