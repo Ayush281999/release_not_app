fd6f6a5dcbb25cc63b796930c8eb12bf2cfb9bb7
Ayush Kumar <ayushk.itpath@gmail.com>
2025-04-06 21:25:41 +0530

    create new api

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/github-webhook', [GitHubWebhookController::class, 'handleGitHubWebhook']);
+// Route::post('/github-webhook', [GitHubWebhookController::class, 'handleGitHubWebhook']);
+Route::post('/github', [GitHubWebhookController::class, 'handleGitHubWebhok']);
